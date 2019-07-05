50250bd84187123b5d8d1e573a0c92514a652c9a
LorenzoDyce <LorenzoDyce@gmail.com>
2019-07-05 18:55:50 -0400

    GraphQL Schema

---
+++ server/schema.js
@@ -0,0 +1,39 @@
+export default `
+type Author {
+  id: ID!
+  firstName: String!
+  lastName:String!
+  posts:[Post!]!
+}
+
+
+type Post {
+  id: ID!
+  title: String
+  content: String!
+  authorId: ID!
+  author: Author!
+}
+
+
+type Query {
+  posts: [Post!]!
+  pot(id: ID!): Post
+  author(id:ID!): Author
+  authors:[Author!]!
+}
+
+
+
+type Mutation {
+  createPost(title:String, content!, authorId: ID!): Post!
+  updatePost(id:ID!, title: String, content:String):  [Int!]!
+  deletePost(id: ID!): Int!
+}
+
+
+
+
+
+
+`;
